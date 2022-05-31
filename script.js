cc46ece4883e70c5001b2d2fcec76e7bbb62810f
Monnylly <monnylly@gmail.com>
2022-05-31 09:16:20 -0300

    feat: criando funcao da cobrinha e area jogo

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 function criarBG() {
-  context.fillStyle = "lightgreen";
+  context.fillStyle = "yellow";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha(){
+  for(i= 0; i< snake.length; i++) {
+    context.fillStyle = "pink"
+    context.fillRect(snake[i].x, snake[i].y, box, box)
+  }
+}
+
 criarBG();
+
+criarCobrinha();
 
 // file:///home/monnylly/Projects_persona/projetos-dio/Jogo-da-cobrinha/index.html
